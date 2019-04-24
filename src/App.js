2f056b1251e1dddc45e79b30906fcd324595bb78
amlane <twelvexstring@gmail.com>
2019-04-24 15:13:50 -0500

    initial push, added data array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
 
+const list = [
+  {
+    task: 'Laundry',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 2,
+    completed: false
+  },  
+  {
+    task: 'Swiffer',
+    id: 1,
+    completed: false
+  }
+];
+
 class App extends React.Component {
+  constructor(){
+    super();
+
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
